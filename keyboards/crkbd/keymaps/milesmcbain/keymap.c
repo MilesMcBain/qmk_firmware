a90a366590ffb3e7e37a356cf8d60ced9527f617
Miles McBain <miles.mcbain@gmail.com>
2021-10-01 20:37:03 +1000

    swap del and bspc

--- keyboards/crkbd/keymaps/milesmcbain/keymap.c
+++ keyboards/crkbd/keymaps/milesmcbain/keymap.c
@@ -28,12 +28,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[L_BASE] = LAYOUT_split_3x6_3(KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,                          KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO, 
                                  KC_TAB, LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F),KC_G, KC_H, RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT, 
                                  KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,                           KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, 
-                                                    LT(4,KC_ESC), LT(1,KC_E), KC_ENT,           KC_DEL, LT(2,KC_SPC), LT(3,KC_BSPC)),
+                                                    LT(4,KC_ESC), LT(1,KC_E), KC_ENT,           KC_BSPC, LT(2,KC_SPC), LT(3,KC_DEL)),
                                             
 	[L_NAV] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_GRV, KC_1, KC_2, KC_3, KC_NO, KC_NO,
                                  KC_TAB, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO,             KC_H, KC_J, KC_K, KC_L, KC_TAB, KC_NO, 
                                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
-                                                      KC_NO, KC_TRNS, KC_ENT,                   KC_DEL, KC_SPC, KC_BSPC),
+                                                      KC_NO, KC_TRNS, KC_NO,                    KC_BSPC, KC_SPC, KC_DEL),
 
 	[L_NUM] = LAYOUT_split_3x6_3(KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
                                  KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                      KC_NO, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, KC_NO, 
