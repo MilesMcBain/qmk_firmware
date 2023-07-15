6059796d69b6e73e67781f3def4bccee362dba90
Miles McBain <miles.mcbain@gmail.com>
2023-07-15 23:23:29 +1000

    whitespace and formatting

--- keyboards/crkbd/keymaps/mcbainanishtro3/keymap.c
+++ keyboards/crkbd/keymaps/mcbainanishtro3/keymap.c
@@ -25,32 +25,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define L_FUN 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[L_BASE] = LAYOUT_split_3x6_3(KC_NO, KC_QUOT, KC_P, KC_U, KC_C, KC_J,                          KC_K, KC_L, KC_D, KC_W, KC_Q, KC_NO, 
-                                 KC_ESC, LGUI_T(KC_A), LALT_T(KC_N), LSFT_T(KC_I), LCTL_T(KC_S),KC_B, KC_V, RCTL_T(KC_H), RSFT_T(KC_T), LALT_T(KC_R), RGUI_T(KC_O), KC_TAB, 
-                                 KC_NO, KC_SCLN, KC_SLSH, KC_Y, KC_F, KC_Z,                     KC_X, KC_M, KC_G, KC_COMM, KC_DOT, KC_NO, 
-                                            KC_ESC, LT(L_FUN, KC_ENT), LT(L_NAV, KC_E),     LT(L_NUM, KC_SPC), LT(L_SYM, KC_BSPC), KC_DEL),
+	[L_BASE] = LAYOUT_split_3x6_3(KC_ESC, KC_QUOT, KC_P, KC_U, KC_C, KC_J,                                       KC_K, KC_L, KC_D, KC_W, KC_Q, KC_TAB,
+                                 KC_ENT, LGUI_T(KC_A), LALT_T(KC_N), LSFT_T(KC_I), LCTL_T(KC_S),LT(L_FUN, KC_B), LT(L_SYM, KC_V), RCTL_T(KC_H), RSFT_T(KC_T), LALT_T(KC_R), RGUI_T(KC_O), KC_BSPC, 
+                                 KC_NO, KC_SCLN, KC_SLSH, KC_Y, KC_F, KC_Z,                                      KC_X, KC_M, KC_G, KC_COMM, KC_DOT, KC_DEL, 
+                                            KC_ESC, LT(L_NAV, KC_E), KC_NO,                                      KC_NO, LT(L_NUM, KC_SPC), KC_NO),
                                             
-	[L_NAV] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_GRV, KC_1, KC_2, KC_3, KC_NO, KC_NO,
-                                 KC_ESC, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO,             KC_H, KC_J, KC_K, KC_L, KC_TAB, KC_TAB, 
-                                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
-                                                      KC_NO, KC_NO, KC_TRNS,                    KC_SPC, KC_BSPC, KC_DEL),
+	[L_NAV] = LAYOUT_split_3x6_3(KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_GRV, KC_1, KC_2, KC_3, KC_NO, KC_TAB,
+                                 KC_ENT, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO,             KC_H, KC_J, KC_K, KC_L, KC_TAB, KC_BSPC, 
+                                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_DEL,
+                                                      KC_NO, KC_TRNS, KC_NO,                    KC_NO, KC_SPC, KC_NO),
 
-	[L_NUM] = LAYOUT_split_3x6_3(KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-                                 KC_ESC, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                      KC_NO, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, KC_TAB, 
-                                 KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-                                                      KC_DOT, KC_0, KC_MINS,                    KC_TRNS, KC_NO, KC_NO),
+	[L_NUM] = LAYOUT_split_3x6_3(KC_ESC, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TAB, 
+                                 KC_0, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                      KC_NO, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, KC_BSPC, 
+                                 KC_DOT, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL, 
+                                                     KC_NO , KC_MINS, KC_NO,                       KC_NO, KC_TRNS, KC_NO),
 
-	[L_SYM] = LAYOUT_split_3x6_3(KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-                                 KC_ESC, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,             KC_NO, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, KC_TAB,
-                                 KC_NO, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                                          KC_LPRN, KC_RPRN, KC_UNDS,            KC_NO, KC_TRNS, KC_NO),
+	[L_SYM] = LAYOUT_split_3x6_3(KC_ESC, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TAB, 
+                                 KC_RPRN, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,             KC_TRNS, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, KC_BSPC,
+                                 KC_LPRN, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL,
+                                                          KC_NO, KC_UNDS, KC_NO,            KC_NO, KC_SPC, KC_NO),
 
-	[L_FUN] = LAYOUT_split_3x6_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_PSCR, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, 
-                                 KC_ESC, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NO,              KC_SLCK, KC_F5, KC_F6, KC_F7, KC_F8, KC_TAB, 
+	[L_FUN] = LAYOUT_split_3x6_3(KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_PSCR, KC_F9, KC_F10, KC_F11, KC_F12, KC_TAB, 
+                                 KC_ENT, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_TRNS,              KC_SLCK, KC_F5, KC_F6, KC_F7, KC_F8, KC_APP, 
                                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_PAUS, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, 
-                                                      KC_NO, KC_TRNS, KC_NO,                    KC_SPC, KC_APP, KC_TAB)
+                                                      KC_NO, KC_E, KC_NO,                    KC_NO, KC_SPC, KC_NO)
 };
 
+// This stops a quick tap of I or T followed by a hold triggering a repeat of the I or T
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LSFT_T(KC_I):
